52b1aa7f454f23c322e5a8059dc706f414a91b7c
Ana Sofía <anasofiasama@gmail.com>
2022-09-02 02:02:26 +0000

    APP

--- app.py
+++ app.py
@@ -5,15 +5,20 @@ import streamlit as st
 
 @st.cache
 
-df_final = pd.read_csv('df_final_topics.csv')
+def load_data():
+    df_final = pd.read_csv('df_final_topics.csv')
 
-df_final['text'] = df_final['text'].astype('str')
-df_final['text_clean'] = df_final['text_clean'].astype('str')
-df_final['text_clean_lemm'] = df_final['text_clean_lemm'].astype('str')
+    df_final['text'] = df_final['text'].astype('str')
+    df_final['text_clean'] = df_final['text_clean'].astype('str')
+    df_final['text_clean_lemm'] = df_final['text_clean_lemm'].astype('str')
 
-df_final['date']=df_final['date'].astype('datetime64')
-df_final['date_short']=df_final['date_short'].astype('datetime64')
-df_final['medio']=df_final['medio'].astype('category')
+    df_final['date']=df_final['date'].astype('datetime64')
+    df_final['date_short']=df_final['date_short'].astype('datetime64')
+    df_final['medio']=df_final['medio'].astype('category')
+    
+    return df_final
+
+df_final = load_data()
 
 indices=pd.DataFrame(df_final.groupby('date_short')['count_DEPU'].mean()).rename(columns={'count_DEPU':'freq_DEPU'})
 indices['freq_DEPUC']=pd.DataFrame(df_final.groupby('date_short')['count_DEPUC'].mean()).rename(columns={'count_DEPUC':'freq_DEPUC'})
