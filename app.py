9db4af5a2f28cc9b88d1a6dc2d84be45950f75c9
Ana Sofía <anasofiasama@gmail.com>
2022-09-02 01:45:14 +0000

    APP

--- app.py
+++ app.py
@@ -4,43 +4,28 @@ import plotly.express as px
 import streamlit as st
 
 @st.cache
-def load_data():
-    url = 'https://raw.githubusercontent.com/rfordatascience/' + \
-    'tidytuesday/master/data/2020/2020-07-07/coffee_ratings.csv'
-    df = pd.read_csv(url)
-    df_interim = df.copy()
-    df_interim = df_interim[['total_cup_points',
-                                'species',
-                                'country_of_origin',
-                                'variety',
-                                'aroma',
-                                'aftertaste',
-                                'acidity',
-                                'body',
-                                'balance',
-                                'sweetness',
-                                'altitude_mean_meters',
-                                'moisture']]
-    df_interim = df_interim.dropna()
-    df_interim['species'] = pd.Categorical(df_interim['species'])
-    df_interim['country_of_origin'] = pd.Categorical(df_interim['country_of_origin'])
-    df_interim['variety'] = pd.Categorical(df_interim['variety'])
-    df_interim['specialty'] = df_interim['total_cup_points'].\
-        apply(lambda x: 'Yes' if x>82.43 else 'No')
-    df = df_interim.copy()
-    return df
 
-df_ch = load_data()
+df_final = pd.read_csv('df_final_topics')
 
-st.title('Coffee dataset explorer')
-st.subheader('Dataframe')
-st.dataframe(df_ch)
-st.subheader('Histograms')
-col1, col2 = st.columns(2)
-fig1 = px.histogram(df_ch, x='aroma')
-fig2 = px.histogram(df_ch, x='aftertaste')
-col1.plotly_chart(fig1, use_container_width=True)
-col2.plotly_chart(fig2, use_container_width=True)
+df_final['text'] = df_final['text'].astype('str')
+df_final['text_clean'] = df_final['text_clean'].astype('str')
+df_final['text_clean_lemm'] = df_final['text_clean_lemm'].astype('str')
+
+df_final['date']=df_final['date'].astype('datetime64')
+df_final['date_short']=df_final['date_short'].astype('datetime64')
+df_final['medio']=df_final['medio'].astype('category')
+
+indices=pd.DataFrame(df_final.groupby('date_short')['count_DEPU'].mean()).rename(columns={'count_DEPU':'freq_DEPU'})
+indices['freq_DEPUC']=pd.DataFrame(df_final.groupby('date_short')['count_DEPUC'].mean()).rename(columns={'count_DEPUC':'freq_DEPUC'})
+
+st.title('Uncertainty indexes')
+#st.subheader('EPOC')
+
+fig = px.line(indices, x=indices.index, y="freq_DEPU",title="DEPU frequency of tweets", width=800, height=400, color_discrete_sequence=px.colors.qualitative.Dark24)
+fig.show()
+
+fig = px.line(indices, x=indices.index, y="freq_DEPUC",title="DEPUC frequency of tweets", width=800, height=400, color_discrete_sequence=px.colors.qualitative.Dark24)
+fig.show()
 
 # Heroku uses the last version of python, but it conflicts with 
 # some dependencies. Low your version by adding a runtime.txt file
